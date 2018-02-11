0ce74103d00e47e7640e5f4cf77bce6a99795fb1
Shilin Victor <chrono.monochrome@gmail.com>
2018-02-11 08:04:37 +0300

    update

--- merge.py
+++ merge.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python2
 
+import os, sys
 import subprocess
+
+DRYRUN = {"--dry-run": "-d"}
+QUIET = {"--quiet": "-q"}
+
+valid_flags = [DRYRUN, QUIET]
 
 def bash_command(cmd):
 	subprocess.Popen(cmd, shell=True, executable='/bin/bash')
@@ -21,13 +27,17 @@ def startswith_any(s, l):
 			return s
 	return ""
 
-def main():
+def main(flags = []):
 	ours_list = [i for i in open("ours.txt", "rb").read().split("\n") if i]
 	theirs_list = [i for i in open("theirs.txt", "rb").read().split("\n") if i]
 	unmerged = [i for i in subprocess.check_output("git diff --name-only --diff-filter=U".split(" ")).split("\n") if i]
 
+	_flag_quiet = QUIET in flags
+	_flag_dryrun = DRYRUN in flags
+
 	if not unmerged:
-		print("No files needs merging.")
+		if not _flag_quiet:
+			print("No files needs merging.")
 		exit()
 
 	to_ours, to_theirs = [], []
@@ -62,10 +72,14 @@ def main():
 		buf += "git commit --no-edit"
 
 	s = "\n\t" + "\n\t".join(unmerged)
-	print(buf)
-	bash_command(buf)
 
-	if unmerged:
+	if not _flag_dryrun:
+		bash_command(buf)
+	elif not _flag_quiet:
+		print(buf)
+
+
+	if not (_flag_dryrun or _flag_quiet) and unmerged:
 		print("The following files needs merging: \n%s\n" % s)
 		try:
 			_start_nano = raw_input("Do you want to open these files in nano? [y]/n\n")
@@ -75,4 +89,12 @@ def main():
 			pass
 
 if __name__ == "__main__":
-	main()
+	flags = []
+	_args = sys.argv
+
+	for flag in valid_flags:
+		alias, key = list(flag.items())[0]
+		if alias in _args or key in _args:
+			flags.append(flag)
+
+	main(flags)
